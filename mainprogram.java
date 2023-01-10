4edf9f33ac351f824307bb408131c7cec083d501
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-01-10 22:55:26 +0530

    Add files via upload

---
+++ mainprogram.java
@@ -0,0 +1,16 @@
+package abstractdemo;
+
+
+    
+
+public class mainprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Teacher t1 = new Teacher("abc","ajiet123","assitant professor",87654,"m.tech",3.5);
+t1.biodata();
+	}
+
+}
+
+
