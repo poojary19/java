99c018f10cbe92c0fac505bf70c30dbb39aa252b
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-02-06 20:08:05 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,17 @@
+package program;
+
+import java.util.Scanner;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner reader = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int number = reader.nextInt();
+	        System.out.println("You entered: " + number);
+	    
+	
+	}
+
+}
