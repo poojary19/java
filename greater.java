e507a2847afd3593bef575364f51b85556a09d14
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-02-07 20:07:19 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,17 @@
+package program;
+
+public class greater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+int y=40;
+if(x>y) {
+	System.out.println("x is greater than y");
+}
+else {
+	System.out.println("y is greater than x");
+}
+	}
+
+}
