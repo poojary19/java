95f55b436c8dc3fbe52a9a66e3ea034bb449bc67
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-02-08 19:50:14 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,12 @@
+package program;
+
+public class logical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+System.out.println(a!=b);
+	}
+
+}
