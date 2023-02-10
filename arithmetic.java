3b78fc325994f10758e0bffeffc09d7c1390a00e
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-02-10 19:24:26 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+package program;
+
+public class arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+System.out.println(a/b);
+	}
+
+}
