556d806da6b71bd2d36bd6009a3b00d3563c2daf
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-02-13 21:26:10 +0530

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,18 @@
+package program;
+
+public class Smallest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int [] arr = new int [] {25, 11, 7, 75, 56};   
+	        int min = arr[0];  
+	        for (int i = 0; i < arr.length; i++) {   
+	           if(arr[i] <min)  
+	               min = arr[i];  
+	        }  
+	        System.out.println("Smallest element present in given array: " + min);  
+	    }  
+	  
+	}
+
+
