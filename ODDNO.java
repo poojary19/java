1fa31be62181608be2e6b3afc93e27b61203f0c5
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-04-04 21:50:09 +0530

    Add files via upload

---
+++ ODDNO.java
@@ -0,0 +1,21 @@
+package program;
+
+import java.util.Scanner;
+
+public class ODDNO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int num=100;
+
+System.out.println("print the 1 to 100 number");
+for(int i=1;i<=num;i++) {
+	if(i%2!=0) {
+		System.out.println(i+"");
+	}
+}
+
+	}
+
+}
