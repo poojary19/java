acd1073d78c1aad9d48e9bc839969eebab518f3f
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-04-07 08:36:35 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,16 @@
+package program;
+
+import java.util.Arrays;
+
+public class sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=new int[] {1,56,89,34,78,34};
+Arrays.sort(arr);
+for(int i=0;i<arr.length;i++) {
+	System.out.println(arr[i]);
+}
+	}
+
+}
