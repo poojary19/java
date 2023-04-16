d92126e57f37f0adc1f9665229009824f957fd12
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-04-16 20:37:16 +0530

    Add files via upload

---
+++ no.java
@@ -0,0 +1,13 @@
+package program;
+
+public class no {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+			double d=12.3;  
+			String s=String.valueOf(d);  
+			System.out.println(s);  
+			 
+	}
+
+}
