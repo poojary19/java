02b1ca23e10e985d11fa58d9017cf1f72452e5b1
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-05-16 21:28:10 +0530

    Add files via upload

---
+++ Ifexample.java
@@ -0,0 +1,16 @@
+package program;
+
+public class Ifexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			    int age=20;  
+			    //checking the age  
+			    if(age>18){  
+			        System.out.print("Age is greater than 18");  
+			    }  
+			}  
+		 
+	}
+
+
