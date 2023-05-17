804b88e0eda2888cf0487380e888310d17371404
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-05-17 21:46:57 +0530

    Add files via upload

---
+++ Mainclass.java
@@ -0,0 +1,22 @@
+package program;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 60;
+
+	    System.out.print("Factors of " + number + " are: ");
+
+	    // loop runs from 1 to 60
+	    for (int i = 1; i <= number; ++i) {
+
+	      // if number is divided by i
+	      // i is the factor
+	      if (number % i == 0) {
+	        System.out.print(i + " ");
+	      }
+	    }
+	}
+
+}
