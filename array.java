a78f13f0cd64fb10c1d04ecbb63b020d265f61ef
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-05-20 16:15:31 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+package program;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+	}
+
+}
