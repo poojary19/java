45b7d7b56be454d54252f747b3586e772089d942
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-05-21 16:22:17 +0530

    Add files via upload

---
+++ comparestring.java
@@ -0,0 +1,17 @@
+package program;
+
+public class comparestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        String style = "Bold";
+        String style2 = "Bold";
+
+        if(style == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+	}
+
+}
