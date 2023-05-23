4f1e2bd50797257d7423210fc7af4ad42fcadb59
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-05-23 22:08:20 +0530

    Add files via upload

---
+++ Naturalno.java
@@ -0,0 +1,14 @@
+package program;
+
+public class Naturalno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,num=10,sum=0;
+for( i=0;i<=num;i++) {
+	sum=sum+i;
+	System.out.println("sum of n natural no"+sum);
+}
+	}
+
+}
